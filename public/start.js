d2666c2d986c08738e1921b823065d91fda3ad08
Roshhl <rochellelawson.98@gmail.com>
2025-06-11 22:30:48 +0100

    index

--- public/start.js
+++ public/start.js
@@ -2,4 +2,7 @@ document.addEventListener('keydown', function (e) {
   if (e.key.toLowerCase() === 'a') {
     window.location.href = './loading.html';
   }
+  else if (e.key.toLowerCase() === 'tap') {
+    window.location.href = './loading.html';
+  }
 });
